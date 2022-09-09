93262f70bc78d946ee2fa1fe694baf9f7d44148d
ibroharun <ibroharun92@gmail.com>
2022-09-09 16:13:34 -0700

    program to print in lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char c;
 
-	for  (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		putcahr(c);
+		putchar(c);
 	}
 
-	for (c = 'A'; c <= 'z'; c++)
+	for (c = 'A'; c <= 'Z'; c++)
 	{
 		putchar(c);
 	}
