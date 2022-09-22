933a6656fd3a099f17ef16dbe8fdf8f05145ead4
ibroharun <ibroharun92@gmail.com>
2022-09-21 17:35:59 -0700

    function to encode a string

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,11 +13,11 @@ char *rot13(char *s)
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = o; s[i] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; j++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == a[i])
+			if (s[i] == a[j])
 			{
 				s[i] = b[j];
 				break;
