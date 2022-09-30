4665858b4a8e6215e04204b3e9363de573bb28cb
ibroharun <ibroharun92@gmail.com>
2022-09-30 09:44:21 -0700

    prints number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - prints the main number of coins to make change for an amount
+ * main - prints the minimum number of coins to make change for an amount 
  * of money
  *
  * @argc: number of command line arguments
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 {
 	int cents, ncoins = 0;
 
-	if (argc == 1 || arg > 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 			cents -= 25;
 		else if (cents >= 10)
 			cents -= 10;
-		else if (cents > 5)
+		else if (cents >= 5)
 			cents -= 5;
 		else if (cents >= 2)
 			cents -= 2;
