4e4432513badc00f6823b7f84b8ecdf4280df168
ibroharun <ibroharun92@gmail.com>
2022-10-03 08:27:34 -0700

    nested loop to make grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,8 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	if (width <= 0 || height <= 0)
-		return (Null);
+		return (NULL);
+
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
