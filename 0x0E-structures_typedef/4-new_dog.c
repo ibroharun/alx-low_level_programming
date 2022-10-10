ce778aa8baef62ab083f08e7cedd31c65ccd735e
ibroharun <ibroharun92@gmail.com>
2022-10-10 06:38:25 -0700

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,10 +2,10 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new odg
+ * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
- * @owner: owner of god
+ * @owner: owner of dog
  *
  * Return: pointer to new dog
  */
