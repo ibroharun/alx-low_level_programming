96f1187e1a3f3a3f37297e797f7a82083c4c0e20
ibroharun <ibroharun92@gmail.com>
2022-10-12 19:19:43 -0700

    3 get functions

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * get_op_func - Compares fucntion with pointer and returns the equal.
+ * get_op_func - Compares function with pointer and returns the equal.
  * @s: points a char
  * Return: Returns the function and the operator when equal
  **/
